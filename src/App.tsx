4e6522e00f2c17825ea233bcaa01dbb53f032de4
mihalay123 <mihalay26@gmail.com>
2024-10-19 13:20:33 +0400

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>This line was deployed using GitHub Actions to GitHub Pages</p>
       </div>
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
