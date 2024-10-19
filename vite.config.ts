4588b19173a65d34fe15e5174e2792e302bccb2a
mihalay123 <mihalay26@gmail.com>
2024-10-19 16:01:05 +0400

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './CNAME',
+          dest: './',
+        },
+        {
+          src: './src/assets/fonts',
+          dest: './assets',
+        },
+      ],
+    }),
+  ],
   base: '/',
 });
